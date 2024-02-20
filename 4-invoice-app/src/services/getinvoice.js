83010892b59bf7f9aa94a25c2af4e0a6ea1471d2
Adolfo Gustavo Velazquez Luna <adolfo.velazquez@coppel.com>
2024-02-20 16:49:46 -0700

    sse agrego lña funcion de sumar nuevo

--- 4-invoice-app/src/services/getinvoice.js
+++ 4-invoice-app/src/services/getinvoice.js
@@ -1,5 +1,14 @@
 import { invoice } from "../data/invoice"
 
 export const getInvoice = () =>{
-    return invoice
+    //calcula el total de los item y se agreaga en la factura
+    /*let total = 0;
+    invoice.items.forEach(item => {
+        total = total + item.price * item.quantity;
+    });*/
+    //hacer el calculo usando calback hace el calculo de sumar los precios de los item
+    const total = invoice.items
+        .map(item => item.price * item.quantity)//operacion de los precios
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    return {...invoice, total};
 }
