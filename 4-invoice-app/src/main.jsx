1ed733996d5f01d5327913a928c2d0a1b59dd006
Adolfo Gustavo Velazquez Luna <adolfo.velazquez@coppel.com>
2024-02-20 16:48:51 -0700

    se importo el archivo ivoivoiceApp

--- 4-invoice-app/src/main.jsx
+++ 4-invoice-app/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {InvoiceApp} from './components/InvoiceApp.jsx'
+import {InvoiceApp} from './InvoiceApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
