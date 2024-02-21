0aef7458982cdf91f2f099a8292c604a15946d7b
Adolfo Gustavo Velazquez Luna <adolfo.velazquez@coppel.com>
2024-02-21 16:00:57 -0700

    Se realizaron cambios en el form y optimizando codigo separando funcion del inputs

--- 4-invoice-app/src/InvoiceApp.jsx
+++ 4-invoice-app/src/InvoiceApp.jsx
@@ -11,12 +11,56 @@ export const InvoiceApp = () => {
 
     const {total,id,name, client, company, items:itemsInitial} = getInvoice();
     //se crea para registrar nuevos item
-    const [productValuet, setProductValue] = useState('');
-    const [pricetValuet, setPricetValue] = useState(0);
-    const [quantityValuet, setQuantityValue] = useState(0);
+    const [productValue, setProductValue] = useState('');
+    const [priceValue, setPriceValue] = useState('');
+    const [quantityValue, setQuantityValue] = useState('');
     //para guardar los items
     const [items, setItems] = useState(itemsInitial);
+    //usa para el contador del id iniciando en 4 porque ya se tiene 3 registros
+    const [counter, setCounter] = useState(4);
 
+    const onProductChange = ({target}) => {
+        console.log(target.value);
+        setProductValue(target.value);
+    };
+
+    
+    const onPriceChange = ({target}) => {
+        console.log(target.value);
+        setPriceValue(target.value);
+    };
+
+    const onQuantityChange = (event) => {
+        console.log(event.target.value);
+        setQuantityValue(event.target.value);
+    };
+    
+    const onInvoiceItemSubmit = (event) => {
+        event.preventDefault();
+        //se realizan las validaciones del formulario
+        if(productValue.trim().length <= 1) return;
+        if(priceValue.trim().length <= 1) return;
+        if(isNaN(priceValue.trim())) {
+            alert('No es un numero '+priceValue)
+            return;
+        }
+        if(quantityValue.trim().length < 1) return;
+        if(isNaN(quantityValue.trim())) return;
+        
+        setItems([...items, {
+            id:counter,
+            product:productValue.trim(),//el trim() funciona para quitar los espacios
+            //covertir enetero con el signo +
+            price:+priceValue.trim(),
+            //convertir entero con parseInt
+            quantity:parseInt(quantityValue.trim(), 10)
+        }]);
+        setProductValue('');
+        setPriceValue('');
+        setQuantityValue('');
+        setCounter(counter+1);
+
+    };
     return (
         <>
             <div className="container my-3">
@@ -41,43 +85,34 @@ export const InvoiceApp = () => {
                     </div>
                 </div>
 
-                <form className="align-items-center card my-3" onSubmit={ event =>{
-                    event.preventDefault();
-                    setItems([...items, {
-                        product:productValuet,
-                        price:pricetValuet,
-                        quantity:quantityValuet
-                    }])
-                }}>
+                <form className="align-items-center card my-3" onSubmit={ onInvoiceItemSubmit }>
                     <h3>Nuevo Item</h3>
                     <div className="row my-3">
                         <div className="col-6">
                             <input 
                             className="form-control" 
-                            type="text" name="product" 
-                            placeholder="Producto" onChange={event => {
-                                console.log(event.target.value);
-                                setProductValue(event.target.value);
-                            }} />
+                            type="text" 
+                            name="product" 
+                            value={productValue}
+                            placeholder="Producto" 
+                            onChange={ onProductChange } />
                         </div>
                         <div className="col-3">
                             <input 
                             className="form-control" 
-                            type="number" 
+                            type="text" 
                             name="price" 
-                            placeholder="$Preio" onChange={event => {
-                                console.log(event.target.value);
-                                setPricetValue(event.target.value);
-                            }} />
+                            value={priceValue}
+                            placeholder="$Preio" 
+                            onChange={onPriceChange} />
                         </div>
                         <div className="col-3">
                             <input className="form-control"
                             type="number" 
                             name="quantity" 
-                            placeholder="Cantidad" onChange={event => {
-                                console.log(event.target.value);
-                                setQuantityValue(event.target.value);
-                            }} /> 
+                            value={quantityValue}
+                            placeholder="Cantidad" 
+                            onChange={event => onQuantityChange(event)} /> 
                         </div>
                     </div>
                     <div className="my-3">
